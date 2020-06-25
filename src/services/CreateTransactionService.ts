b4251651d01f77941807811c581e15ccef804885
Janes Roberto <kadoshmt2@gmail.com>
2020-06-25 17:48:41 -0400

    funcional version

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,68 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: 'string';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categortRepository = getRepository(Category);
+
+    // verify if type is valid
+    if (!['income', 'outcome'].includes(type)) {
+      throw new AppError(
+        'Type transactions is invalid. Allowed income or outcome only.',
+      );
+    }
+
+    // verify if user have funds to perform an outcome
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+
+      if (value > balance.total) {
+        throw new AppError(
+          "You don't have sufficient money to realize this transaction.",
+        );
+      }
+    }
+
+    // Verify if the category entry is new
+    let findCategory = await categortRepository.findOne({
+      title: category.trim(),
+    });
+
+    if (!findCategory) {
+      findCategory = await categortRepository.create({
+        title: category.trim(),
+      });
+      await categortRepository.save(findCategory);
+    }
+
+    const transaction = await transactionsRepository.create({
+      title,
+      type,
+      value,
+      category: findCategory,
+      category_id: findCategory.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,30 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import { isUuid } from 'uuidv4';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    // Verify if id is a valid uuid key
+    const validUuid = isUuid(id);
+
+    if (!validUuid) {
+      throw new AppError('Invalid Id.');
+    }
+
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionsRepository.findOne({ id });
+
+    // Verify if DB found a registry
+    if (!transaction) {
+      throw new AppError('Transaction not found.');
+    }
+
+    await transactionsRepository.delete(id);
   }
 }
 
